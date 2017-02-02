6f53d40c05096aabbc30b7da0f132e9caf7fc3aa
mac <admin@mahadev.local>
2017-02-02 14:21:46 +0530

    commit

---
+++ Build/Intermediates/Cube.build/Debug-iphonesimulator/Copy to Cube.build/Objects-normal/x86_64/ShareViewController.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/admin/Documents/CubeBackUp/cube-master\ 2__16-aug/Copy\ to\ Cube/ShareViewController.m \
+  /Users/admin/Documents/CubeBackUp/cube-master\ 2__16-aug/Copy\ to\ Cube/ShareViewController.h
